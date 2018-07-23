f747e3586780e5ccdb2960fed93a828e6374f5f4
Perth Charernwattanagul <perth.charern@Perths-MacBook-Pro.local>
2018-07-23 20:24:18 +0700

    add wrong code

---
+++ teaching_materials/buildKGramDictionary.1.js
@@ -0,0 +1,27 @@
+// Code #1
+// Please run this code with
+// input = "abcabd"
+// numCharsLookBack = 2
+
+function buildKGramDictionary( input, numCharsLookBack ) 
+{
+   var kGramDictionary = {}; // Initialize kGramDictionary to an empty dictionary
+   for ( var i = 0; i < input.length - numCharsLookBack; i++)
+   {
+       var currentSubstring = ""; // This is an empty string
+       for ( var j = 0; j < numCharsLookBack; j++ )
+       {
+           ////////////////// Note the difference between this and Code #2 //////////////////
+           currentSubstring = currentSubstring + input[i + j];
+           //////////////////////////////////////////////////////////////////////////////////
+       }
+
+       if ( !( currentSubstring in kGramDictionary ) ){ // Checks if current substring is NOT in kGramDictionary 
+           kGramDictionary[currentSubstring] = []; // [] means an empty array
+       }
+          
+       kGramDictionary[currentSubstring].push( input[i + numCharsLookBack] ); 
+   }
+
+   return kGramDictionary;
+}
